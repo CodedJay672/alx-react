9f7eadfb7d4fb5d229fad162912bff272c1203a2
codedjay672 <emmajoel6456@gmail.com>
2023-05-16 18:45:36 +0100

    update jquery syntax

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,12 +1,7 @@
 import $ from "jquery";
 
 $(document).ready(function() {
-  const para1 = $("<p>Holberton Dashboard</p>");
-  const para2 = $("<p>Dashboard data for the students</p>");
-  const para3 = $("<p>Copyright - Holberton School</p>");
-
-
-  $("body").append(para1);
-  $("body").append(para2);
-  $("body").append(para3);
+	$('body').append('<p>Holberton Dashboard</p>');
+	$('body').append('<p>Dashboard data for the students</p>');
+	$('body').append('<p>Copyright - Holberton School</p>');
 });
