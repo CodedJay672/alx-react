3c0389ecc00d5e6b79694751f0787aea01010a8e
codedjay672 <emmajoel6456@gmail.com>
2023-05-16 18:09:46 +0100

    update files

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-$(document).ready(function () {
+$(document).ready(function() {
   const para1 = $("<p>Holberton Dashboard</p>");
   const para2 = $("<p>Dashboard data for the students</p>");
   const para3 = $("<p>Copyright - Holberton School</p>");
@@ -9,4 +9,4 @@ $(document).ready(function () {
   $("body").append(para1);
   $("body").append(para2);
   $("body").append(para3);
-}
+});
