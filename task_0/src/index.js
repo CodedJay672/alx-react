09f274d5b4a2d000b7378b3ec11364f9d16d6e3b
codedjay672 <emmajoel6456@gmail.com>
2023-05-16 01:26:45 +0100

    upload config, js and html files

---
+++ task_0/src/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+  const para1 = $("<p>Holberton Dashboard</p>");
+  const para2 = $("<p>Dashboard data for the students</p>");
+  const para3 = $("<p>Copyright - Holberton School</p>");
+
+
+  $("body").append(para1);
+  $("body").append(para2);
+  $("body").append(para3);
+}
