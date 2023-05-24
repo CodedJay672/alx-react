d4a81bbfcb1bdf0ffa1eb513ac064a3d8b9e3206
codedjay672 <emmajoel6456@gmail.com>
2023-05-24 18:07:46 +0100

    include a form interface

---
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import close from './close-icon.png';
+import { getLatestNotification } from './utils';
+import './Notifications.css';
+
+function Notifications() {
+  return (
+    <div id="root-nofications">
+      <div className='Notifications'>
+        <button style={{float: 'right', backgroundColor: "white", border: "none"}} aria-label='close' onClick={console.log("Close button has been clicked")}>
+          <img src={close} alt='icon' style={{width: .5 + 'rem', height: .5 + 'rem'}} />
+        </button>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data-priority="default">New course available</li>
+          <li data-priority="urgent">New resume available</li>
+          <li dangerouslySetInnerHTML={{__html: getLatestNotification()}} />
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Notifications;
