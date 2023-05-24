c38cfc282c2d9a621dd7d55abbba4cbc062bc1ac
codedjay672 <emmajoel6456@gmail.com>
2023-05-24 10:29:47 +0100

    update notification component and update the rending from App.js

---
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Notifications.css';
+
+function Notifications() {
+  return (
+    <div id="root-nofications">
+      <div className='Notifications'>
+        <p>Here is the list of notifications</p>
+      </div>
+    </div>
+  );
+}
+
+export default Notifications;
