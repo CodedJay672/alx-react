790ae51f8a31f60b9f909d7192c1c4e74158832f
codedjay672 <emmajoel6456@gmail.com>
2023-05-29 14:03:20 +0100

    include babel presets for older browsers

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -32,6 +32,20 @@ module.exports = {
       },
     ],
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
   devServer: {
     hot: true,
   },
