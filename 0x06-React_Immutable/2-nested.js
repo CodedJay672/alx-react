5cc2a82b4e91a70bec091ee7eda03983302e6675
codedjay672 <emmajoel6456@gmail.com>
2023-06-29 12:40:51 +0100

    hot fix

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -3,5 +3,5 @@ import { Map } from "./node_modules/immutable/dist/immutable";
 export default function accessImmutableObject(object, array) {
   const map = Map(object);
 
-  return (map.getIn([array[0], array[1]]));
+  return Map(map.getIn(array, undefined));
 }
