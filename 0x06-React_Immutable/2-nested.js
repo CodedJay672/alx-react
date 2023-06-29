42523f46516ca0914c04ec88990feced8600bd57
codedjay672 <emmajoel6456@gmail.com>
2023-06-29 11:15:44 +0100

    upload nested.js

---
+++ 0x06-React_Immutable/2-nested.js
@@ -0,0 +1,7 @@
+import { fromJS } from "./node_modules/immutable/dist/immutable";
+
+export default function accessImmutableObject(object, array) {
+  const map = fromJS(object);
+
+  return getIn([array[0], array[1]]);
+}
