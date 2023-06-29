16e637823581424b7fb5f27a38d73ac7b56626fa
codedjay672 <emmajoel6456@gmail.com>
2023-06-29 13:36:46 +0100

    update function

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,5 +1,5 @@
 import { Map } from "./node_modules/immutable/dist/immutable";
 
-export const mergeDeelyElements = (page1, page2) => {
+export default mergeDeelyElements = (page1, page2) => {
   return Map(page1).mergeDeep(Map(page2));
 }
