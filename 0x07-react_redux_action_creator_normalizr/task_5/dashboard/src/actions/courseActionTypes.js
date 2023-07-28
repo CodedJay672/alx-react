c666d9cd462b0050431070c627672faf4423a235
codedjay672 <emmajoel6456@gmail.com>
2023-07-28 02:37:07 +0100

    add base files

---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/actions/courseActionTypes.js
@@ -0,0 +1,4 @@
+// This file list some action types
+
+export const SELECT_COURSE = 'SELECT_COURSE';
+export const UNSELECT_COURSE = 'UNSELECT_COURSE';
---
+++ 0x07-react_redux_action_creator_normalizr/task_5/dashboard/src/schema/output.js
@@ -0,0 +1,3 @@
+import { getAllNotificationsByUser } from "./notifications";
+
+console.log(getAllNotificationsByUser("5debd764a7c57c7839d722e9"));
