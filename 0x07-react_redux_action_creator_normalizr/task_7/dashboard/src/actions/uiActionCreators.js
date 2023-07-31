0a07eaf602a38aadbb2fb8845783c9118e9ee7b8
codedjay672 <emmajoel6456@gmail.com>
2023-07-31 15:26:49 +0100

    add more action creators

--- 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -4,7 +4,9 @@ import {
   LOGIN,
   LOGOUT,
   DISPLAY_NOTIFICATION_DRAWER,
-  HIDE_NOTIFICATION_DRAWER
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE
 } from "./uiActionTypes";
 
 import { bindActionCreators } from 'redux';
@@ -20,20 +22,44 @@ export const login = bindActionCreators((email, password) => {
 }, dispatch)
 
 
-export const logout = () => {
+export const logout = bindActionCreators(() => {
   return {
     type: LOGOUT
   }
-}
+}, dispatch)
 
-export const displayNotificationDrawer = () => {
+export const displayNotificationDrawer = bindActionCreators(() => {
   return {
     type: DISPLAY_NOTIFICATION_DRAWER
   }
-}
+}, dispatch)
 
-export const hideNotificationDrawer = () => {
+export const hideNotificationDrawer = bindActionCreators(() => {
   return {
     type: HIDE_NOTIFICATION_DRAWER
   }
+}, dispatch)
+
+export const loginSuccess = () => {
+  return {
+    type: LOGIN_SUCCESS
+  }
+}
+
+export const loginFailure = () => {
+  return {
+    type: LOGIN_FAILURE
+  }
+}
+
+
+export const loginRequest = () => {
+  dispatch(login());
+  fetch('/dist/login-success.json')
+  .then((res) => {
+    if(!res.ok) {
+      dispatch(loginFailure());
+    }
+    dispatch(loginSuccess());
+  })
 }
